fdea6b6457d85620c399dcd22d70e7651951ee5a
igorFrotte <igorfrotte@gmail.com>
2025-09-14 00:44:01 -0300

    connection ready

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import prisma from "./db/db.js";
 
 dotenv.config();
 
@@ -8,7 +9,9 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get("/status", (req, res) => {
+server.get("/status", async (req, res) => {
+  const users = await prisma.aluno.findMany();
+  console.log(users);
   res.sendStatus(200);
 });
 
